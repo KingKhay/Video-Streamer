0752732e3a60a42c8dd53c0f742d517220438b7e
KingKhay <ebenezerbuabeng60@gmail.com>
2023-03-20 11:34:57 +0000

    added the spring email dependency

--- src/main/java/com/khaydev/videostream/app/controller/admin/AdminController.java
+++ src/main/java/com/khaydev/videostream/app/controller/admin/AdminController.java
@@ -1,6 +1,8 @@
 package com.khaydev.videostream.app.controller.admin;
 
+import com.khaydev.videostream.app.model.Role;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/admin")
 public class AdminController {
 
-    public ResponseEntity<?> createRole(){
+    public ResponseEntity<?> createRole(@RequestBody Role role){
         return null;
     }
 }
