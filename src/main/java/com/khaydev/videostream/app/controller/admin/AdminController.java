404bb4356364015491a80270fee3b35cd285d7da
KingKhay <ebenezerbuabeng60@gmail.com>
2023-03-20 16:26:44 +0000

    amended the admin controller

--- src/main/java/com/khaydev/videostream/app/controller/admin/AdminController.java
+++ src/main/java/com/khaydev/videostream/app/controller/admin/AdminController.java
@@ -1,15 +1,16 @@
 package com.khaydev.videostream.app.controller.admin;
 
 import com.khaydev.videostream.app.model.Role;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/admin")
 public class AdminController {
 
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    @PostMapping("/")
     public ResponseEntity<?> createRole(@RequestBody Role role){
         return null;
     }
