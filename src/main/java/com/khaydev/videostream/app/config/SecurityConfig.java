46526ac2dd8a38911d964e72787469cb1c8740c5
KingKhay <ebenezerbuabeng60@gmail.com>
2023-03-21 08:47:27 +0000

    refactored the security config to assign my customUserdetailsService to the authenticationManager bean

--- src/main/java/com/khaydev/videostream/app/config/SecurityConfig.java
+++ src/main/java/com/khaydev/videostream/app/config/SecurityConfig.java
@@ -1,13 +1,18 @@
 package com.khaydev.videostream.app.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -16,18 +21,24 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class SecurityConfig {
 
+
+    @Qualifier("customUserDetailsService")
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http.csrf().disable();
         http.sessionManagement()
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and()
-                .authorizeHttpRequests(authorize -> authorize.requestMatchers(HttpMethod.POST, "/auth/signup").permitAll()
-                        .requestMatchers(HttpMethod.GET, "/auth/signin").authenticated())
-                .authorizeHttpRequests(authorize -> authorize.requestMatchers("/users").hasRole("ADMIN")
-                        .requestMatchers("/items").hasAnyRole("ADMIN", "USER")
-                        .anyRequest().authenticated())
-                .httpBasic();
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.authorizeHttpRequests((authz) -> authz
+                        .requestMatchers("/api/login").permitAll()
+                        .requestMatchers("api/register").permitAll()
+                        .requestMatchers("/api/admin/**").hasAuthority("ROLE_ADMIN")
+                        .anyRequest().authenticated());
         return http.build();
     }
 
@@ -37,7 +48,10 @@ public class SecurityConfig {
     }
 
     @Bean
-    public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
-        return config.getAuthenticationManager();
+    public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+        AuthenticationManagerBuilder authenticationManagerBuilder =
+                http.getSharedObject(AuthenticationManagerBuilder.class);
+        authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+        return authenticationManagerBuilder.build();
     }
 }
