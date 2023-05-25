35193bf7c0ec3b0d1298954f15cde1ce90f9c0b6
KingKhay <ebenezerbuabeng60@gmail.com>
2023-05-25 11:21:51 +0000

    refactored register user service method to check if user already exist

--- src/main/java/com/khaydev/videostream/app/service/auth/IAuthService.java
+++ src/main/java/com/khaydev/videostream/app/service/auth/IAuthService.java
@@ -5,6 +5,7 @@ import com.khaydev.videostream.app.dto.LoginDTO;
 import com.khaydev.videostream.app.dto.LoginResponse;
 import com.khaydev.videostream.app.dto.RegisterResponse;
 import com.khaydev.videostream.app.event.RegistrationSuccessEvent;
+import com.khaydev.videostream.app.exception.user.UserAlreadyExistException;
 import com.khaydev.videostream.app.model.Role;
 import com.khaydev.videostream.app.model.User;
 import com.khaydev.videostream.app.repository.RoleRepository;
@@ -14,13 +15,16 @@ import com.khaydev.videostream.app.utils.jwt.JwtUtils;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 
 @Service
@@ -41,6 +45,10 @@ public class IAuthService implements AuthService{
 
     @Override
     public RegisterResponse register(User user) {
+        Optional<User> existingUser = this.repository.findUserByUsername(user.getUsername());
+        if(existingUser.isPresent()){
+            throw new UserAlreadyExistException("username already exists");
+        }
         String roleUser = "ROLE_USER";
         Role userRole = roleRepository.getRoleByName(roleUser);
         user.getRoles().add(userRole);
@@ -58,18 +66,22 @@ public class IAuthService implements AuthService{
 
     @Override
     public LoginResponse login(LoginDTO loginDTO) {
-        Authentication authenticate = authenticationManager
-                .authenticate(new UsernamePasswordAuthenticationToken(
-                        loginDTO.getUsername(),
-                        loginDTO.getPassword()
-                ));
-        CustomUserDetails userDetails = (CustomUserDetails) authenticate.getPrincipal();
-        List<String> roles = userDetails.getAuthorities()
-                .stream()
-                .map(GrantedAuthority::getAuthority)
-                .toList();
-        String token = jwtUtils.createJwt(userDetails.getUsername(), roles);
+        try{
+            Authentication authenticate = authenticationManager
+                    .authenticate(new UsernamePasswordAuthenticationToken(
+                            loginDTO.getUsername(),
+                            loginDTO.getPassword()
+                    ));
+            CustomUserDetails userDetails = (CustomUserDetails) authenticate.getPrincipal();
+            List<String> roles = userDetails.getAuthorities()
+                    .stream()
+                    .map(GrantedAuthority::getAuthority)
+                    .toList();
+            String token = jwtUtils.createJwt(userDetails.getUsername(), roles, new Date(System.currentTimeMillis() * 10 * 60 * 1000));
 
-        return new LoginResponse(userDetails.getUsername(),token);
+            return new LoginResponse(userDetails.getUsername(),token, "login successful");
+        }catch(Exception ex){
+            throw new BadCredentialsException("Invalid username or password");
+        }
     }
 }
