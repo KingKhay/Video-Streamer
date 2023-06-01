7ac848f3f183d280cefc65a5b914b1e846433083
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-06-01 13:40:42 +0000

    added the findPasswordToken by Token method in the repository of the PasswordResetToken entity

--- src/main/java/com/khaydev/videostream/app/repository/PasswordTokenRepository.java
+++ src/main/java/com/khaydev/videostream/app/repository/PasswordTokenRepository.java
@@ -3,5 +3,9 @@ package com.khaydev.videostream.app.repository;
 import com.khaydev.videostream.app.model.PasswordResetToken;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface PasswordTokenRepository extends JpaRepository<PasswordResetToken, Long> {
+
+    Optional<PasswordResetToken> findPasswordResetTokenByToken(String token);
 }
