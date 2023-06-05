28f820f160046eb60742a5c741efeb2c6d7c47e6
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-06-05 10:22:16 +0000

    added validations to email entity

--- src/main/java/com/khaydev/videostream/app/model/Email.java
+++ src/main/java/com/khaydev/videostream/app/model/Email.java
@@ -1,5 +1,6 @@
 package com.khaydev.videostream.app.model;
 
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,8 +12,13 @@ import lombok.NoArgsConstructor;
 @Builder
 public class Email {
 
+    @Pattern(regexp = "^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$", message = "Invalid email format")
     private String from;
+
+    @Pattern(regexp = "^\\w+([\\.-]?\\w+)*@\\w+([\\.-]?\\w+)*(\\.\\w{2,3})+$", message = "Invalid email format")
     private String to;
+
+    @Pattern(regexp = "\\S+.*\\S+", message = "email subject cannot have leading or trailing spaces")
     private String subject;
     private String text;
 }
