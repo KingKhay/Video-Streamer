c1f7cc366b8c2e70e4e30bae562edf1717a20216
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-07-05 15:53:21 +0000

    refactor comment service to make use of object mapper

--- src/main/java/com/khaydev/videostream/app/service/comment/ICommentService.java
+++ src/main/java/com/khaydev/videostream/app/service/comment/ICommentService.java
@@ -1,30 +1,28 @@
 package com.khaydev.videostream.app.service.comment;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.khaydev.videostream.app.dto.CommentDTO;
 import com.khaydev.videostream.app.repository.CommentRepository;
 import com.khaydev.videostream.app.utils.mapper.EntityObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
 
 @Service
+@RequiredArgsConstructor
 public class ICommentService implements CommentService{
 
     private final CommentRepository commentRepository;
 
-    private final EntityObjectMapper objectMapper;
-
-    public ICommentService(CommentRepository commentRepository, EntityObjectMapper objectMapper) {
-        this.commentRepository = commentRepository;
-        this.objectMapper = objectMapper;
-    }
+    private final ObjectMapper mapper;
 
     @Override
     public List<CommentDTO> findCommentsByVideo(UUID id) {
         return commentRepository.findCommentsByVideoId(id)
                 .stream()
-                .map(objectMapper::convertCommentToCommentDTO)
+                .map(comment -> mapper.convertValue(comment, CommentDTO.class))
                 .toList();
     }
 
