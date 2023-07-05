873ae21d25bf9ea6624134f91e06bb61dd526fe9
Ebenezer Buabeng <ebenezerbuabeng60@gmail.com>
2023-07-05 16:24:20 +0000

    wrote unit test to find user by Id

--- src/test/java/com/khaydev/videostream/app/service/user/IUserServiceTest.java
+++ src/test/java/com/khaydev/videostream/app/service/user/IUserServiceTest.java
@@ -1,13 +1,22 @@
 package com.khaydev.videostream.app.service.user;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.khaydev.videostream.app.dto.UserDTO;
+import com.khaydev.videostream.app.model.User;
 import com.khaydev.videostream.app.repository.UserRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
+import java.util.Optional;
+import java.util.UUID;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 class IUserServiceTest {
 
@@ -23,5 +32,27 @@ class IUserServiceTest {
     @BeforeEach
     void setUp(){
         MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    @DisplayName("Find_Users_By_Id")
+    void testFindUserByIdSuccess(){
+        UUID id = UUID.randomUUID();
+
+        User user = new User();
+        user.setUsername("Khay");
+
+        UserDTO userDTO = new UserDTO();
+        userDTO.setUsername("Khay");
+
+        when(repository.findById(id)).thenReturn(Optional.of(user));
+        when(mapper.convertValue(any(), eq(UserDTO.class))).thenReturn(userDTO);
+
+        UserDTO result = userService.findUserById(id);
+
+        assertNotNull(result);
+        assertEquals(user.getUsername(), result.getUsername());
+
+        verify(repository, times(1)).findById(id);
     }
 }
